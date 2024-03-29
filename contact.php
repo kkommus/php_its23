058eb724b53342a3eba03bc9e9772eea286d6d8f
kkommus <keiti.k6mmus@gmail.com>
2024-03-29 10:22:39 +0200

    uu

--- contact.php
+++ contact.php
@@ -1,1 +1,15 @@
+<?php
+include("header.php");
+?>
+<br>
+<div class="container"
+
+    <h1>Kontakt</h1>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae aspernatur ullam consequatur animi illum. Commodi accusamus, porro recusandae, in quisquam cupiditate eos minus obcaecati magni voluptates fuga illo ut reprehenderit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae aspernatur ullam consequatur animi illum. Commodi accusamus, porro recusandae, in quisquam cupiditate eos minus obcaecati magni voluptates fuga illo ut reprehenderit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae aspernatur ullam consequatur animi illum. Commodi accusamus, porro recusandae, in quisquam cupiditate eos minus obcaecati magni voluptates fuga illo ut reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae aspernatur ullam consequatur animi illum. Commodi accusamus, porro recusandae, in quisquam cupiditate eos minus obcaecati magni voluptates fuga illo ut reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae aspernatur ullam consequatur animi illum. Commodi accusamus, porro recusandae, in quisquam cupiditate eos minus obcaecati magni voluptates fuga illo ut reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae aspernatur ullam consequatur animi illum. Commodi accusamus, porro recusandae, in quisquam cupiditate eos minus obcaecati magni voluptates fuga illo ut reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae aspernatur ullam consequatur animi illum. Commodi accusamus, porro recusandae, in quisquam cupiditate eos minus obcaecati magni voluptates fuga illo ut reprehenderit. </p>
+
+</div>
+<br>        
+
+<?php
+include("footer.php");
+?>
--- pood.php
+++ pood.php
@@ -3,10 +3,14 @@ include("header.php");
 ?>
 
 
+<br>
+<div class="container"
 
-<h1>Pood</h1>
+    <h1>Pood</h1>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae aspernatur ullam consequatur animi illum. Commodi accusamus, porro recusandae, in quisquam cupiditate eos minus obcaecati magni voluptates fuga illo ut reprehenderit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae aspernatur ullam consequatur animi illum. Commodi accusamus, porro recusandae, in quisquam cupiditate eos minus obcaecati magni voluptates fuga illo ut reprehenderit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae aspernatur ullam consequatur animi illum. Commodi accusamus, porro recusandae, in quisquam cupiditate eos minus obcaecati magni voluptates fuga illo ut reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae aspernatur ullam consequatur animi illum. Commodi accusamus, porro recusandae, in quisquam cupiditate eos minus obcaecati magni voluptates fuga illo ut reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae aspernatur ullam consequatur animi illum. Commodi accusamus, porro recusandae, in quisquam cupiditate eos minus obcaecati magni voluptates fuga illo ut reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae aspernatur ullam consequatur animi illum. Commodi accusamus, porro recusandae, in quisquam cupiditate eos minus obcaecati magni voluptates fuga illo ut reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae aspernatur ullam consequatur animi illum. Commodi accusamus, porro recusandae, in quisquam cupiditate eos minus obcaecati magni voluptates fuga illo ut reprehenderit. </p>
 
+</div>
+<br>        
 
 <?php
 include("footer.php");
